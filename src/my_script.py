a656eea722c251c36d811749f033ebe7b6392814
ncl7 <ncl7@njit.edu>
2019-10-14 20:02:31 -0400

    Task: made comment for python file

--- src/my_script.py
+++ src/my_script.py
@@ -1,5 +1,7 @@
 # Sample taken from pyStrich GitHub repository
 # https://github.com/mmulqueen/pyStrich
+# Nicole Lim's comment on my_script.py
+
 from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
